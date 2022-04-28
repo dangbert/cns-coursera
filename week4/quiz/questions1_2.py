052a3244fc7c32a790c4ece441b08158ae5b3f36
Daniel Engbert <end1@umbc.edu>
2022-04-28 12:25:33 -0700

    Finish most of quiz4.

--- week4/quiz/questions1_2.py
+++ week4/quiz/questions1_2.py
@@ -2,21 +2,29 @@
 import math
 
 def q1():
+  """
+  note: I added this full problem to my notes
+  https://beta.engbert.me/p/sci/cns/coursera.md
+  """
+  print("\n------q1---------")
   res = -0.1 * math.log(0.1, 2) + (-0.9 * math.log(0.9, 2))
   print(f"q1: {res:0.4f}\n")
+  return res
 
-# TODO: doesn't look like this answer is right:
-def q2():
-  # total entropy of responses:
-  totalEntropyR = -0.055*math.log(0.055, 2) - 0.845*math.log(0.845, 2)
+def q2(a1):
+  """computing Mutal information. see equation for MI(S,R) in notes"""
+  print("\n------q2---------")
+  # total entropy of responses (comes from question1)
+  totalEntropyR = a1
+
   # average noise entropy:
-  avgEntropyN = -0.1 * (0.5*math.log(0.5, 2)) * (0.5*math.log(0.5, 2)) + \
-    -0.9 * (1/18 * math.log(1/18, 2)) * (17/18 * math.log(17/18, 2))
+  avgEntropyN = -0.1 * (0.5*math.log(0.5, 2) + 0.5*math.log(0.5, 2)) + \
+    -0.9 * (1/18*math.log(1/18, 2) + 17/18*math.log(17/18, 2))
 
   print(f"totalEntropyR = {totalEntropyR:0.4f}")
   print(f"avgEntropyN = {avgEntropyN:0.4f}")
   res = totalEntropyR - avgEntropyN
   print(f"q2: {res:0.4f}\n")
 
-q1()
-q2()
+a1 = q1()
+q2(a1)
--- week4/quiz/tuning.py
+++ week4/quiz/tuning.py
@@ -4,7 +4,7 @@ import math
 import pickle
 from matplotlib import pyplot as plt
 
-def q7():
+def q7_8():
   with open('tuning_3.4.pkl', 'rb') as f:
     data = pickle.load(f)
   print('tuning data is a dict with keys:')
@@ -23,25 +23,60 @@ def q7():
     nData = data[key]
 
     tuningData[key] = []
-    for stimIndex in range(nData.shape[1]): # 24 stimuli values
+    for stimIndex in range(nData.shape[1]): # 24 unique stimuli values
+      """"
+      each column corresponds to a given stimulus and has 100 entries
+      (each entry being the firing rate over a single 10 sec trial of the given stimulus)
+      """
       # compute average firing rate across all trials with this particular stimulus
-      avgResponse = np.mean(nData[:, stimIndex])
-      tuningData[key].append(avgResponse)
+      avgRes = np.mean(nData[:, stimIndex])
+      tuningData[key].append(avgRes)
 
-    # plot tuning curve for this neuron
+    ## plot tuning curve for this neuron
     #   we want to see how its average firing rate (response) varies based on the stimuli
     splot = axis[plotMap[n-1]] # AxesSubPlot
-    #plt.clf()
     splot.plot(data['stim'], tuningData[key])
     #splot.legend()
     splot.set_xlabel('stimulus value (air velocity direction)')
-    splot.set_ylabel('average firing rate Hz ({} trials)'.format(len(data['stim'])))
-    splot.set_title('Tuning Curve: {}'.format(key))
+    splot.set_ylabel('average firing rate Hz (n={})'.format(len(data['stim'])))
+    splot.set_title('{}: Tuning Curve'.format(key))
 
   fig.tight_layout(h_pad=4)
   plt.gcf().set_size_inches(11, 8.5)
-  plt.savefig("tuning_curves.png".format(key), dpi=400)
+  plt.savefig("tuning_curves.png", dpi=400)
   #plt.show()
+  print("wrote tuning_curves.png to disk!")
 
 
-q7()
+  # now lets investigate the poisson(ness) of each neuron
+  plt.clf()
+  fig, axis = plt.subplots(2, 2)
+  pData = {} # poisson (or not) data
+  for n in range(1, 5):
+    key = f"neuron{n}"
+    nData = data[key]
+
+    # compute list of variances (one for each stimulus)
+    #   (multiply by 10 to convert trial firing rate to spike count)
+    variance = np.var(nData * 10, axis=0)
+    avgSpikes = np.mean(nData * 10, axis=0)
+    pData[key] = {'var': variance, 'avg': avgSpikes}
+
+    ## plot variance vs avg spikes for each neuron to evalute poisson(ness)
+    splot = axis[plotMap[n-1]] # AxesSubPlot
+    splot.scatter(avgSpikes, variance)
+    #splot.legend()
+    splot.set_xlabel('avg. spike count')
+    splot.set_ylabel('variance of spike count')
+    splot.set_title('{}: Spike count average vs variance'.format(key))
+
+  fig.tight_layout(h_pad=4)
+  plt.gcf().set_size_inches(11, 8.5)
+  fName = "poisson.png"
+  plt.savefig(fName, dpi=400)
+  plt.title('Poisson(ness) Evaluation of Neurons')
+  print(f"wrote {fName} to disk!")
+
+
+if __name__ == "__main__":
+  q7_8()
